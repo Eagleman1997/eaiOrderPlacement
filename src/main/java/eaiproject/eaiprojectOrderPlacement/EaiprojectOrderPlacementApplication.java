dcd40059a1e30e05da3d493ff7e591ca07a88467
adrianmathys <adrianmathys@adrianscbookpro.edu.ds.fhnw.ch>
2018-10-15 10:59:32 +0200

    init

---
+++ src/main/java/eaiproject/eaiprojectOrderPlacement/EaiprojectOrderPlacementApplication.java
@@ -0,0 +1,12 @@
+package eaiproject.eaiprojectOrderPlacement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaiprojectOrderPlacementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaiprojectOrderPlacementApplication.class, args);
+	}
+}
---
+++ src/test/java/eaiproject/eaiprojectOrderPlacement/EaiprojectOrderPlacementApplicationTests.java
@@ -0,0 +1,16 @@
+package eaiproject.eaiprojectOrderPlacement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EaiprojectOrderPlacementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
